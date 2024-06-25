12edfb6f89812fcb8f999ec4869409be969fbe7b
Shubham Rajput <rajput.shubham37@gmail.com>
2024-06-26 03:33:44 +0530

    handle request

--- app/main.py
+++ app/main.py
@@ -2,14 +2,34 @@
 import socket
 
 
+def handle_request(client_socket):
+    client_socket.recv(1024)
+
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.send(response.encode())
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()
+
+    try:
+        while True:
+            print("Waiting for a connection...")
+            client_socket, addr = server_socket.accept()
+            handle_request(client_socket)
+            client_socket.close()
+
+    except KeyboardInterrupt:
+        print("^C received, shutting down the web server...")
+    finally:
+        server_socket.close()
+        print("Server closed the server!")
+
+
+
+
 
 
 if __name__ == "__main__":
